cdf810682e78ce4b32aaea5e9ea24d3aef1f6a5e
siddharth ghosh <siddharth.kido@gmail.com>
2025-10-23 23:14:48 -0700

    auto tuner updates + translational PIDF done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -15,11 +15,23 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+    static double robotMassV1 = 0.938;
+    static double forwardV = 77.47656300615128; // test 2: 79.07251679918099
+    static double strafeV = 58.13652697505066; // test 2: 59.71897433734531 || 57.41994932375924
+    static double forwardZeroPowerAccel = -34.687022166256604; // test 2: -34.687022166256604 || -33.74762702215598 || abhays test: -26.917226893087108
+    static double lateralZeroPowerAccel = -61.163609551169515; // test 2: -63.878670347567514 || -61.163609551169515
+
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(9.938)
-            .forwardZeroPowerAcceleration(-26.917226893087108)
-            .lateralZeroPowerAcceleration(-56.43135188306015)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.08, 0, 0, 0))
+            .mass(robotMassV1)
+            .forwardZeroPowerAcceleration(forwardZeroPowerAccel)
+            .lateralZeroPowerAcceleration(lateralZeroPowerAccel)
+            .translationalPIDFCoefficients(new PIDFCoefficients(
+                    0.075, // good range is 0.75 to 0.85 cant rlly know yet till we try actual pathing
+                    0,
+                    0.11, // 0.01 to 0.12
+                    0.023 // good range is 0.21 to 0.28.
+                    )
+            )
             .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0, 0.01));
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
@@ -33,8 +45,8 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(77.12999309181)
-            .yVelocity(58.13652697505066)   ;
+            .xVelocity(forwardV)
+            .yVelocity(strafeV); // test 2: 59.71897433734531 || 57.41994932375924
 
 
 
